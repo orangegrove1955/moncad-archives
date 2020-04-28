156e8caf50a8b0607de4a788ccb9c732883540a1
orangegrove_1955 <orangegrove_1955@hotmail.com>
2020-04-28 22:22:33 +1000

    Added video and lazy loading to Gallery

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,29 +1,38 @@
 import React, { useEffect, useState } from "react";
-import Menu from "./Menu";
+import LazyLoad from "react-lazy-load";
 
-const Gallery = ({ match, location }) => {
-  const [images, setImages] = useState([]);
+const Gallery = ({ location }) => {
+  let [images, setImages] = useState([]);
 
-  const getImages = async _ => {
-    console.log("getting" + location.pathname);
+  const getImages = async (_) => {
     const data = await fetch(
       `https://api.moncad.com.au/getFiles/public/${location.pathname}`
     );
     const json = await data.json();
     setImages(json);
-    console.log(location);
   };
 
   useEffect(() => {
     getImages();
   }, []);
 
+  let video = images.filter((image) => image.endsWith(".mp4"));
+  images = images.filter((image) => !image.endsWith(".mp4"));
+
   return (
     <div>
+      {video.length > 0 && (
+        <LazyLoad key={video[0]} offset={1000}>
+          <video src={`https://api.moncad.com.au/${video[0]}`} controls></video>
+        </LazyLoad>
+      )}
       <div className="grid">
-        {images.map(image => {
-          console.log(image.endsWith(".mp4"));
-          return <img src={`https://api.moncad.com.au/${image}`} alt=""></img>;
+        {images.map((image) => {
+          return (
+            <LazyLoad key={image} offset={1000}>
+              <img src={`https://api.moncad.com.au/${image}`} alt=""></img>
+            </LazyLoad>
+          );
         })}
       </div>
     </div>
